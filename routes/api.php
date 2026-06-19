141c0280c8f690205a39822e7b4878e42f9e707d
thilina <thilinaudayanga1010@gmail.com>
2026-06-19 06:45:05 +0530

    create user register API endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-    return json_encode([
-        "status" => "success",
-        "massage" => "success",
-    ]);
-});
+Route::get('/register',[UserController::class,'userRegister']);
